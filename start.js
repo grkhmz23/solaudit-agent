270b8bb214983f1b7cc215e068984b9f9bec6ef8
Gorkhmaz Beydullayev <151032776+grkhmz23@users.noreply.github.com>
2026-02-12 14:47:33 +0000

    fix: use npx next start instead of direct bin

--- start.js
+++ start.js
@@ -1,20 +1,16 @@
 import { spawn } from "child_process";
 
-// Start Next.js
-const web = spawn("node", ["node_modules/.bin/next", "start", "-p", "3000"], {
+const web = spawn("npx", ["next", "start", "-p", "3000"], {
   cwd: "./apps/web",
   stdio: "inherit",
   env: { ...process.env },
 });
 
-// Start Worker
 const worker = spawn("node", ["apps/worker/dist/index.js"], {
   stdio: "inherit",
   env: { ...process.env },
 });
 
-// If either exits, kill both
 web.on("exit", (code) => { console.log("[web] exited", code); process.exit(code ?? 1); });
 worker.on("exit", (code) => { console.log("[worker] exited", code); process.exit(code ?? 1); });
-
 process.on("SIGTERM", () => { web.kill(); worker.kill(); });
